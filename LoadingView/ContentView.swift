a34be5e320a7b2f5a11e636754bb2776283374bb
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-19 10:27:49 +0200

    Project kickoff

---
+++ LoadingView/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  LoadingView
+//
+//  Created by MUGABE RICHARD on 19/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoadingView/LoadingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingViewApp.swift
+//  LoadingView
+//
+//  Created by MUGABE RICHARD on 19/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LoadingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
