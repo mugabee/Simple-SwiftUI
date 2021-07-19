d7e1b233c378d978184fa4ce351ce812790879dd
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-19 13:41:53 +0200

    Project KickOff

---
+++ NavigationView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationView
+//
+//  Created by MUGABE RICHARD on 19/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by MUGABE RICHARD on 19/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
