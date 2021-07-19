194cf80440b90f7c7e6819ca4bcb4b83b6253430
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-19 10:47:08 +0200

    Creating sample page

--- LoadingView/ContentView.swift
+++ LoadingView/ContentView.swift
@@ -8,7 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    var body: some View {
+        ZStack {
+            Color(.red)
+                .ignoresSafeArea()
+            Text("THis Example Page")
+                .font(.title)
+                .foregroundColor(.white)
+                .bold()
+                .padding()
+            
+        }
     }
 }
 
