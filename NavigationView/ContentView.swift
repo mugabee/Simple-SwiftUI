110c314f2a88b7513c5c6a3dda0f045ef33a23df
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-19 14:16:21 +0200

    Creating shapes and Texts

--- NavigationView/ContentView.swift
+++ NavigationView/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Circle()
+                .frame(width:100, height: 100)
+                .foregroundColor(.blue)
+            Text("1")
+                .foregroundColor(.white)
+                .frame(width: 250, height: 250, alignment: .center)
+                .font(.title)
+                
+           
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CircleNumberView: View {
+    Var colors: Color
+    var Number: Int
+    
+    var body: some View {
+        ZStack
     }
 }
