ee8152d54aebf0f900b3178ddb3ec7db8375a177
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-22 21:09:48 +0200

    Initial Commit

---
+++ SwiftUi Form/SwiftUi_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_FormApp.swift
+//  SwiftUi Form
+//
+//  Created by MUGABE RICHARD on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
